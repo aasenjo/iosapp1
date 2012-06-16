004898de27a7d9eb9c20f4e85ff5aa1674f403ce
Alejandro Asenjo <aasenjo@Alejandros-MacBook-Pro.local>
2012-06-15 21:07:26 -0500

    Initial Commit

---
+++ HelloTest1Tests/HelloTest1Tests.h
@@ -0,0 +1,13 @@
+//
+//  HelloTest1Tests.h
+//  HelloTest1Tests
+//
+//  Created by Alejandro Asenjo on 6/15/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface HelloTest1Tests : SenTestCase
+
+@end
---
+++ HelloTest1Tests/HelloTest1Tests.m
@@ -0,0 +1,32 @@
+//
+//  HelloTest1Tests.m
+//  HelloTest1Tests
+//
+//  Created by Alejandro Asenjo on 6/15/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import "HelloTest1Tests.h"
+
+@implementation HelloTest1Tests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in HelloTest1Tests");
+}
+
+@end
